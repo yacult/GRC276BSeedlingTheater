e2cf52a5f084c06a648fa178926e1f73727667f2
yacult <105097554+yacult@users.noreply.github.com>
2023-05-02 21:01:38 -0700

    Push

---
+++ wp-login.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Log In &lsaquo; Seedling Theater Company &#8212; WordPress</title>
+	<meta name="robots" content="noindex, follow">
+
+            <script data-no-defer="1" data-ezscrex="false" data-cfasync="false" data-pagespeed-no-defer>const ctPublicFunctions = {"_ajax_nonce":"c20d6fb5ed","_rest_nonce":"0be1273947","_ajax_url":"\/Seedling_Theater_Company\/wp-admin\/admin-ajax.php","_rest_url":"http:\/\/localhost\/Seedling_Theater_Company\/wp-json\/","data__cookies_type":"none","data__ajax_type":"rest","text__wait_for_decoding":"Decoding the contact data, let us a few seconds to finish. Anti-Spam by CleanTalk.","cookiePrefix":""}</script>
+        
+            <script data-no-defer="1" data-ezscrex="false" data-cfasync="false" data-pagespeed-no-defer>const ctPublic = {"_ajax_nonce":"c20d6fb5ed","settings__forms__check_internal":0,"settings__forms__check_external":0,"settings__forms__search_test":1,"blog_home":"http:\/\/localhost\/Seedling_Theater_Company\/","pixel__setting":"3","pixel__enabled":false,"pixel__url":"https:\/\/moderate6.cleantalk.org\/pixel\/b8571e1fda56fe6160aabd903dc9c46e.gif","data__email_check_before_post":1,"data__cookies_type":"none","data__key_is_ok":false,"data__visible_fields_required":true,"data__to_local_storage":[]}</script>
+        <!--n2css--><script src="http://localhost/Seedling_Theater_Company/wp-includes/js/jquery/jquery.min.js?ver=3.6.3" id="jquery-core-js"></script>
+<script src="http://localhost/Seedling_Theater_Company/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.0" id="jquery-migrate-js"></script>
+<script data-pagespeed-no-defer src="http://localhost/Seedling_Theater_Company/wp-content/plugins/cleantalk-spam-protect/js/apbct-public-bundle.min.js?ver=6.9.1" id="ct_public_functions-js"></script>
+<link rel="stylesheet" id="dashicons-css" href="http://localhost/Seedling_Theater_Company/wp-includes/css/dashicons.min.css?ver=6.2" media="all">
+<link rel="stylesheet" id="buttons-css" href="http://localhost/Seedling_Theater_Company/wp-includes/css/buttons.min.css?ver=6.2" media="all">
+<link rel="stylesheet" id="forms-css" href="http://localhost/Seedling_Theater_Company/wp-admin/css/forms.min.css?ver=6.2" media="all">
+<link rel="stylesheet" id="l10n-css" href="http://localhost/Seedling_Theater_Company/wp-admin/css/l10n.min.css?ver=6.2" media="all">
+<link rel="stylesheet" id="login-css" href="http://localhost/Seedling_Theater_Company/wp-admin/css/login.min.css?ver=6.2" media="all">
+<link rel="stylesheet" id="ct_public_css-css" href="http://localhost/Seedling_Theater_Company/wp-content/plugins/cleantalk-spam-protect/css/cleantalk-public.min.css?ver=6.9.1" media="all">
+	<meta name="referrer" content="strict-origin-when-cross-origin">
+		<meta name="viewport" content="width=device-width">
+		</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
+	<script type="text/javascript">document.body.className = document.body.className.replace('no-js','js');</script>
+		<div id="login">
+		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="http://localhost/Seedling_Theater_Company/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Username or Email Address</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" autocomplete="username">
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Password</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" autocomplete="current-password" spellcheck="false">
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Show password">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Remember Me</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In">
+									<input type="hidden" name="redirect_to" value="http://localhost/Seedling_Theater_Company/wp-admin/?simply_static_page=135570">
+									<input type="hidden" name="testcookie" value="1">
+			</p>
+		</form>
+
+					<p id="nav">
+				<a href="http://localhost/Seedling_Theater_Company/my-account/lost-password/">Lost your password?</a>			</p>
+					<script type="text/javascript">function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }</script>
+				<p id="backtoblog">
+			<a href="http://localhost/Seedling_Theater_Company/">&larr; Go to Seedling Theater Company</a>		</p>
+			</div>
+			<script id="zxcvbn-async-js-extra">var _zxcvbnSettings = {"src":"http:\/\/localhost\/Seedling_Theater_Company\/wp-includes\/js\/zxcvbn.min.js"};</script>
+<script src="http://localhost/Seedling_Theater_Company/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script src="http://localhost/Seedling_Theater_Company/wp-includes/js/dist/vendor/wp-polyfill-inert.min.js?ver=3.1.2" id="wp-polyfill-inert-js"></script>
+<script src="http://localhost/Seedling_Theater_Company/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.11" id="regenerator-runtime-js"></script>
+<script src="http://localhost/Seedling_Theater_Company/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script src="http://localhost/Seedling_Theater_Company/wp-includes/js/dist/hooks.min.js?ver=4169d3cf8e8d95a3d6d5" id="wp-hooks-js"></script>
+<script src="http://localhost/Seedling_Theater_Company/wp-includes/js/dist/i18n.min.js?ver=9e794f35a71bb98672ae" id="wp-i18n-js"></script>
+<script id="wp-i18n-js-after">
+wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
+</script>
+<script id="password-strength-meter-js-extra">var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};</script>
+<script src="http://localhost/Seedling_Theater_Company/wp-admin/js/password-strength-meter.min.js?ver=6.2" id="password-strength-meter-js"></script>
+<script src="http://localhost/Seedling_Theater_Company/wp-content/plugins/the-events-calendar/common/src/resources/js/underscore-before.js"></script>
+<script src="http://localhost/Seedling_Theater_Company/wp-includes/js/underscore.min.js?ver=1.13.4" id="underscore-js"></script>
+<script src="http://localhost/Seedling_Theater_Company/wp-content/plugins/the-events-calendar/common/src/resources/js/underscore-after.js"></script>
+<script id="wp-util-js-extra">var _wpUtilSettings = {"ajax":{"url":"\/Seedling_Theater_Company\/wp-admin\/admin-ajax.php"}};</script>
+<script src="http://localhost/Seedling_Theater_Company/wp-includes/js/wp-util.min.js?ver=6.2" id="wp-util-js"></script>
+<script id="user-profile-js-extra">var userProfileL10n = {"user_id":"0","nonce":"26503cdfb7"};</script>
+<script src="http://localhost/Seedling_Theater_Company/wp-admin/js/user-profile.min.js?ver=6.2" id="user-profile-js"></script>
+	<div class="clear"></div>
+	</body>
+	</html>
